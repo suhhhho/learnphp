5d9268df86e7ebf7e27d0f31e46011150e55ab98
Sofja <sofja.portnova@tptlive.ee>
2025-09-05 09:46:32 +0300

    Add simple types examples

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+
+echo "Hello World\n";
+echo ('Hello world');
+$name = 'Sofja';
+echo "\nHello $name\n";
+
+$var = 'Hello';
+var_dump($var);
+var_dump(strlen($var));
+
+$var = 1;
+var_dump($var);
+
+$var = 1.5;
+var_dump($var);
+
+var_dump(PHP_INT_MAX);
+var_dump(PHP_FLOAT_MAX);
+
+$var = true;
+var_dump($var);
+
+$bar = null;
+var_dump($bar)
+?>
