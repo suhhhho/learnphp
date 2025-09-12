4a5912b705934e0885f1238484a6edd2966028ff
Sofja <sofja.portnova@tptlive.ee>
2025-09-12 10:55:59 +0300

    add function example

--- index.php
+++ index.php
@@ -1,36 +1,51 @@
 <?php
 
-class Box {
-    public $width;
-    public $height;
-    public $lenght;
+function hello() {
+    var_dump('hello');
+}
 
-    public function volume(){
-        return $this ->width * $this ->height * $this->lenght;
+hello();
+hello();
+hello();
+hello();
+
+
+function helloName($name='Unknown') {
+    var_dump("hello $name");
+}
+
+helloName('Kaspar');
+helloName('Martin');
+helloName();
+
+function sum($a, $b) {
+    return $a+$b;
+}
+
+var_dump(sum(1,4));
+$answer = sum(6,5);
+
+function biggerThanTen($n) {
+    if($n > 10){
+        return 'Bigger';
+    }
+    return 'Smaller';
+    var_dump('asdasdasdasd'); // never runs
+}
+
+var_dump(biggerThanTen(11));
+var_dump(biggerThanTen(4));
+
+function anything(...$args) {
+    var_dump($args);
+}
+anything(1, 2, 3, 4, 5, 6, 7);
+
+function recursive($n){
+    if($n<10){
+        var_dump($n);
+        recursive($n+1);
     }
 }
 
-$num1 = 1;
-$num2 = &$num1;
-$num = 2;
-var_dump($num1);
-
-$box1 = new Box();
-$box1 ->width = 1;
-$box2 = clone $box1;
-$box2 ->width = 2;
-var_dump($box1 -> width);
-var_dump($box1, $box2);
-
-$array = [1, 2, 3, 4, 5];
-
-for($i=0;$i<count($array); $i++){
-    $num = $array[$i];
-    $num *= 2;
-}
-
-foreach($array as &$num){
-    $num *= 2;
-}
-
-var_dump($array);
+recursive(0);
