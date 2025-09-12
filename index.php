d0dcb58b72e00ca5d1bf6bc819c1551983e17a33
Sofja <sofja.portnova@tptlive.ee>
2025-09-12 09:35:58 +0300

    add if and switch case examples

--- index.php
+++ index.php
@@ -1,34 +1,54 @@
 <?php
 
-$bool = true && false;
-$bool = false && true;
-$bool = false && false;
-$bool = true && true;
-var_dump($bool);
-$bool = true || false;
-$bool = false || true;
-$bool =true || true;
-$bool = false || false;
-var_dump($bool);
+$i = 5;
+if($i > 10){
+    var_dump('bigger');
+} elseif ($i == 10){
+    var_dump('equals');
+} else {
+    var_dump('smaller');
+}
 
-$bool = !false;
-var_dump($bool);
-$bool = !true;
-var_dump($bool);
+$day = (int) date ('w', startotime('8.09.2025'));
 
-$bool = (true || false && true) && !(!false || false && true );
-var_dump($bool);
-$bool = false;
+var_dump($day);
 
-$bool = 10 > 4;
-$bool = 10 < 4;
-$bool = 10 > 10;
-$bool = 10 < 10;
-$bool = 10 == 10;
-$bool = 10 == '10';
-$bool = 10 === '10';
-$bool = 10 != '10';
-$bool = 10 !== '10';
-$bool = 10 <= 10;
-$bool = 10 >= 10;
-var_dump($bool);
+if($day == 1) {
+    var_dump('Esmaspäev');
+} else if($day == 2) {
+    var_dump('Teisipäev');
+}else if($day == 3) {
+    var_dump('Kolmapäev');
+}else if($day == 4) {
+    var_dump('Nejapäev');
+}else if($day == 5) {
+    var_dump('Reede');
+}else if($day == 6) {
+    var_dump('Laupäaev');
+}else if($day == 7) {
+    var_dump('Pühapäev');
+}
+
+switch($day) {
+    case 1:
+        var_dump('Esmaspäev');
+        break;
+    case 2:
+        var_dump('Teisipäev');
+        break;
+    case 3:
+        var_dump('Kolmapäev');
+        break;
+    case 4:
+        var_dump('Neljapäev');
+        break;
+    case 5:
+    case 6:
+        var_dump('Pidupäev');
+        break;
+    case 7:
+        var_dump('Pühapäev');
+        break;
+    default:
+    var_dump('Imelik päev');
+}
