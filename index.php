298acccefa5c9592e14e8d3939b8fcf1cbbb8194
Sofja <sofja.portnova@tptlive.ee>
2025-09-12 11:33:43 +0300

    add visibilty

--- index.php
+++ index.php
@@ -1,30 +1,47 @@
 <?php
 
 class Box {
-    public $width;
+    private $width;
     public $height;
     public $lenght;
 
-    public function volume(){
-        return $this ->width * $this-> height * $this -> lenght;
+    public function setWidth($width) {
+        if(is_numeric($width) && $width > 0){
+            $this->width = $width;
+        } else {
+            throw new Exception('You are an idiot');
+        }
+        
     }
 
+    public function getWidth(){
+        return $this->width;
+    }
+
+    public function volume(){
+        return $this->width * $this->height * $this->lenght;
+    }
 }
 
 class MetalBox extends Box {
-    public $material = 'Metal';
+    public $material = 'Metal🤘';
     public $weightPerUnit = 2;
 
+    public function test(){
+        var_dump($this->lenght);
+    }
+
     public function mass() {
-        return $this -> volume() * $this ->weightOerUnit;
+        return $this->volume() * $this->weightPerUnit;
     }
 }
 
 $metalBox1 = new MetalBox();
-$metalBox1->width = 10;
+$metalBox1->setWidth(32);
+//$metalBox1->test();
+var_dump($metalBox1->getWidth());
 $metalBox1->height = 20;
 $metalBox1->lenght = 30;
 
 var_dump($metalBox1->volume());
-var_dump($metalBox1->mass());
 var_dump($metalBox1);
