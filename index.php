46f669a010b6ed6e38936c9a87e04308bec29c2a
Sofja <sofja.portnova@tptlive.ee>
2025-09-12 10:40:18 +0300

    add class and object examples

--- index.php
+++ index.php
@@ -1,43 +1,27 @@
 <?php
 
-for($i=0; $i<10; $i++){
-    var_dump($i);
+class Box {
+    public $width;
+    public $height;
+    public $lenght;
+
+    public function volume(){
+        return $this ->width * $this ->height * $this->lenght;
+    }
 }
 
-for($i=9; $i>=0; $i--){
-    var_dump($i);
-}
-for($i=1; $i<1_000_000; $i*=2){
-    var_dump($i);
-}
+$box1 = new Box();
+$box1 ->width= 10;
+$box1 ->height= 20;
+$box1 ->lenght= 30;
+var_dump($box1);
+var_dump($box1 ->volume());
 
-$start = time();
-$i = 0;
-while(time()<$start+1){
-    $i++;
-}
-var_dump($i);
-$start = time();
-for($i=0; time()<$start+1;$i++){
+$box2 = new Box();
+$box2->width= 40;
+$box2 ->height= 50;
+$box2 ->lenght= 60;
+var_dump($box2);
+var_dump($box2 ->volume());
 
-}
-var_dump($i);
-
-$i = 10;
-while($i<10){
-    var_dump('WHILE');
-}
-
-do{
-    var_dump('DO');
-}while($i<10);
-
-$array = ['Apples', 'Cherries', 'Pears', 'Apricot'];
-
-foreach($array as $fruit){
-    var_dump($fruit);
-}
-
-foreach($array as $key=>$fruit){
-    var_dump("$key=>$fruit");
-}
+var_dump($box1);
